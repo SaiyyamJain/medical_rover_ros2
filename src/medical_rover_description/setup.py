f48b09a5a52d0f1d2735f3a011c7f6ea10d66739
Saiyyam Jain <saiyyamjain9@gmail.com>
2026-07-08 22:59:03 +0530

    Initial ROS2 medical rover description

---
+++ src/medical_rover_description/setup.py
@@ -0,0 +1,48 @@
+from glob import glob
+import os
+from setuptools import find_packages, setup
+
+package_name = 'medical_rover_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+    (
+        'share/ament_index/resource_index/packages',
+        ['resource/' + package_name],
+    ),
+    (
+        'share/' + package_name,
+        ['package.xml'],
+    ),
+    (
+        os.path.join('share', package_name, 'launch'),
+        glob('launch/*.launch.py'),
+    ),
+    (
+        os.path.join('share', package_name, 'urdf'),
+        glob('urdf/*'),
+    ),
+    (
+        os.path.join('share', package_name, 'rviz'),
+        glob('rviz/*'),
+    ),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='saiyyam',
+    maintainer_email='saiyyam@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
